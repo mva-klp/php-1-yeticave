27be3e19ac5f5dd96eb09a423eaaad29e10e5adc
vladimir <markov.pr@yandex.ru>
2023-03-29 17:09:55 +0300

    added func for get time for timer

--- data.php
+++ data.php
@@ -15,36 +15,42 @@ $goods = [
     'title' => '2014 Rossignol District Snowboard',
     'category' => $categories['boards'],
     'price' => '10999',
-    'img_url' => 'img/lot-1.jpg'
+    'img_url' => 'img/lot-1.jpg',
+    'expiry_date' => '2023-03-29'
   ],
   [
     'title' => 'DC Ply Mens 2016/2017 Snowboard',
     'category' => $categories['boards'],
     'price' => '159999',
-    'img_url' => 'img/lot-2.jpg'
+    'img_url' => 'img/lot-2.jpg',
+    'expiry_date' => '2023-04-02'
   ],
   [
     'title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
     'category' => $categories['attachment'],
     'price' => '8000',
-    'img_url' => 'img/lot-3.jpg'
+    'img_url' => 'img/lot-3.jpg',
+    'expiry_date' => '2023-04-12'
   ],
   [
     'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
     'category' => $categories['boots'],
     'price' => '10999',
-    'img_url' => 'img/lot-4.jpg'
+    'img_url' => 'img/lot-4.jpg',
+    'expiry_date' => '2023-05-01'
   ],
   [
     'title' => 'Куртка для сноуборда DC Mutiny Charocal',
     'category' => $categories['clothing'],
     'price' => '7500',
-    'img_url' => 'img/lot-5.jpg'
+    'img_url' => 'img/lot-5.jpg',
+    'expiry_date' => '2023-07-13'
   ],
   [
     'title' => 'Маска Oakley Canopy',
     'category' => $categories['other'],
     'price' => '5400',
-    'img_url' => 'img/lot-6.jpg'
+    'img_url' => 'img/lot-6.jpg',
+    'expiry_date' => '2023-04-26'
   ]
 ];
--- functions.php
+++ functions.php
@@ -7,3 +7,17 @@ function format_price($price)
   }
   return $price . ' ₽';
 }
+
+function get_time_left($exp_date)
+{
+  $current_datetime = date_create("now");
+  $final_date = date_create($exp_date);
+  if ($final_date > $current_datetime) {
+    $diff = date_diff($current_datetime, $final_date);
+    $hours = str_pad($diff->h + $diff->days * 24, 2, "0", STR_PAD_LEFT);
+    $minutes = str_pad($diff->i, 2, "0", STR_PAD_LEFT);
+    return ['hours' => $hours, 'minutes' => $minutes];
+  } else {
+    return ['hours' => '00', 'minutes' => '00'];
+  }
+}
